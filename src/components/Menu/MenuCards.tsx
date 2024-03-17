e9b62634e844caeb557937f75372af05f99e6799
Micaela Jimenez <micajimenez2000@gmail.com>
2024-03-17 17:41:18 -0500

    ruta menu y section menu

---
+++ src/components/Menu/MenuCards.tsx
@@ -0,0 +1,32 @@
+import './../../styles/MenuCards.css';
+import BlueBtn from '../shared/BlueBtn';
+import { menuCardOptionsProps } from '../../interfaces/MenuProps';
+import { Link } from 'react-router-dom';
+
+const MenuCards = ({ menuCardOptions }: any) => {
+  return (
+    <section className="MenuCards">
+      {menuCardOptions.map((menuOption: any) => {
+        return (
+          <article className="flex flex-col width-100 position-relative">
+            <Link to={menuOption.link}>
+              <div className="imageContainer">
+                <img src={menuOption.img} alt="menu1" />
+              </div>
+
+              <div className="flex flex-center">
+                <BlueBtn text="Ver todos" />
+              </div>
+            </Link>
+
+            <Link to={menuOption.link}>
+              <p className="textRed">{menuOption.text}</p>
+            </Link>
+          </article>
+        );
+      })}
+    </section>
+  );
+};
+
+export default MenuCards;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
-export {default as Root} from './Root'
-export {default as Home} from './Home'
-export {default as Promociones} from './Promociones'
-export {default as Cupones} from './Cupones'
-export {default as Error404} from './404/Error404'
+export { default as Root } from './Root';
+export { default as Home } from './Home';
+export { default as Promociones } from './Promociones';
+export { default as Cupones } from './Cupones';
+export { default as Error404 } from './404/Error404';
+export { default as Menu } from './Menu';
