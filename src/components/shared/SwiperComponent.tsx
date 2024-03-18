287dd6d98c2b07c94d14db3df04f0cfcaf0a9400
Renee Magallanes <rene22797@gmail.com>
2024-03-18 17:36:56 -0500

    add custom swiper

---
+++ src/components/shared/SwiperComponent.tsx
@@ -0,0 +1,14 @@
+import { Swiper } from "swiper/react"
+import { SwiperProps } from "../../interfaces"
+
+import 'swiper/swiper-bundle.css';
+
+const SwiperComponent: React.FC<SwiperProps> = ({children, options, className}) => {
+  return (
+    <Swiper {...options} className={className}>
+      {children}
+    </Swiper>
+  )
+}
+
+export default SwiperComponent
