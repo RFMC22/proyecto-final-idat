f5b9d87f893b76e8cba76aad4ec3bd92db963212
Renee Magallanes <rene22797@gmail.com>
2024-03-29 11:42:14 -0500

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/proyecto-final-idat/'
+  base: 'https://rfmc22.github.io/proyecto-final-idat/'
 })
